cb140e76e7f2a110d556b3279f7651650fd09373
Ulrich AIOUNOU <aiounouu@gmail.com>
2024-04-22 19:36:52 +0200

    add:conteriser variables d'environement

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10
+
+EXPOSE 8000
+
+WORKDIR /app
+
+RUN pip install pipenv 
+
+COPY Pipfile Pipfile.lock ./
+
+RUN pipenv install --system
+
+COPY . .
+
+CMD  ["make","dev-con"]
---
+++ src/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file="src/.env",env_file_encoding = 'utf-8',extra = "allow")
+    database_name: str 
+    database_user: str 
+    database_host: str 
+    database_password: str 
