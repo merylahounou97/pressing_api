f94fa65dfbe019451441e96296ea561fe88e03e7
merylahounou97 <ahounoumeryl@yahoo.fr>
2024-04-30 17:02:35 +0200

    add creataion account

--- src/sql/customer/customer_service.py
+++ src/sql/customer/customer_service.py
@@ -3,8 +3,24 @@ from sqlalchemy.orm import Session
 from .customer_model import Customer_model
 from .customer_schema import Customer_schema, Customer_create_schema
 import uuid
+from passlib.hash import bcrypt
+from fastapi import HTTPException
+
+
+
+def check_existing_customer(db: Session, phone_number: str, email):
+    user_by_phone_number = db.query(Customer_model).filter(Customer_model.phone_number == phone_number).first()
+    user_by_email = db.query(Customer_model).filter(Customer_model.email == email).first()
+    if user_by_phone_number and user_by_email:
+        return True
+    return False
+
 
 def create_customer(db: Session, customerCreate: Customer_create_schema):
+    if check_existing_customer(db, customerCreate.phone_number, customerCreate.email):
+        raise HTTPException(status_code=400, detail="Phone number or email already registered")
+    
+    hashed_password = bcrypt.hash(customerCreate.password)
     db_user = Customer_model(
         id= str(uuid.uuid4()),
         email =customerCreate.email,
@@ -12,7 +28,8 @@ def create_customer(db: Session, customerCreate: Customer_create_schema):
         first_name =customerCreate.first_name,
         phone_number =customerCreate.phone_number,
         address =customerCreate.address,
-        password =customerCreate.password
+        password =hashed_password 
+        
     )
     db.add(db_user)
     db.commit()
--- src/sql/person/person_model.py
+++ src/sql/person/person_model.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import  Column,  String
+# from pydantic import EmailStr
 
 class Person_model:
     __tablename__ = "persons"
     id = Column(String, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    last_name = Column(String)
-    first_name = Column(String)
-    phone_number = Column(String)
-    address = Column(String)
+    email = Column(String(150), unique=True, index=True)
+    last_name = Column(String(100))
+    first_name = Column(String(100))
+    phone_number = Column(String(50))
+    address = Column(String(255))
     password = Column(String)
+    
+    # @staticmethod
+    # def validate_email(email: str) -> EmailStr:
+    #     return EmailStr(email)
